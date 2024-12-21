9815837a355910f82b98a9bd0345f17ebe35db9b
JonasRiber <riber16@gmail.com>
2024-12-21 17:55:55 +0100

    loads of changes

--- find_all_repeats.py
+++ find_all_repeats.py
@@ -5,11 +5,10 @@ def find_repeats(sequence):
     """
     btrs = []
     n = len(sequence)
-    for length in range(1, n // 2 + 1):  # Maximum repeat length is n/2
-        for i in range(n - 2 * length + 1):  # Search each possible pair of repeats
+    for length in range(1, n // 2 + 1):  #maximum repeat length is n/2
+        for i in range(n - 2 * length + 1):  # search each possible pair of repeats
             repeat1 = sequence[i:i+length]
             repeat2 = sequence[i+length:i+2*length]
-            # Check if they are the same, allowing for mutations (e.g. up to 1 mutation allowed here)
-            if repeat1 == repeat2:  # Allow 1 mutation
+            if repeat1 == repeat2: # if same, we report it
                 btrs.append((sequence[i:i+2*length], length))
     return btrs
